ee86360a14af93215634f8825f47c36f96fe9c4c
Neoblast1996 <nadal.arthur@gmail.com>
2016-04-15 22:39:23 +0200

    -

--- apps/app_explorer/script.js
+++ apps/app_explorer/script.js
@@ -55,10 +55,12 @@ var app_explorer =
                                 var toAppend = "";
                                 var name = "";
                                 var actions = "";
-                                var actionsFolder = "<img src='apps/app_explorer/images/actions/delete.svg' class='action' onclick='app_explorer.actions.delete(this)' />&nbsp;"+
-                                                    "<img src='apps/app_explorer/images/actions/rename.svg' class='action' onclick='app_explorer.actions.rename(this)' />&nbsp;"+
-                                                    "<img src='apps/app_explorer/images/actions/properties.svg' class='action' onclick='app_explorer.actions.infos(this)' />&nbsp;";
-                                var actionFile = actionsFolder + "<img src='apps/app_explorer/images/actions/download.svg' class='action' onclick='app_explorer.actions.download(this)' />&nbsp;";
+                                var actionsFolder = "<img src='apps/app_explorer/images/actions/delete.svg' class='action' onclick='app_explorer.actions.delete(this)' />&nbsp;&nbsp;"+
+                                                    "<img src='apps/app_explorer/images/actions/rename.svg' class='action' onclick='app_explorer.actions.rename(this)' />&nbsp;&nbsp;"+
+                                                    "<img src='apps/app_explorer/images/actions/copy.svg' class='action' onclick='app_explorer.actions.copy(this)' />&nbsp;&nbsp;"+
+                                                    "<img src='apps/app_explorer/images/actions/cut.svg' class='action' onclick='app_explorer.actions.cut(this)' />&nbsp;&nbsp;"+
+                                                    "<img src='apps/app_explorer/images/actions/properties.svg' class='action' onclick='app_explorer.actions.infos(this)' />&nbsp;&nbsp;";
+                                var actionFile = actionsFolder + "<img src='apps/app_explorer/images/actions/download.svg' class='action' onclick='app_explorer.actions.download(this)' />";
                                 var onclickAction = "";
                                 
                                 for(line_key in content)
@@ -84,7 +86,7 @@ var app_explorer =
                                                     onclickAction = "app_explorer.actions.open(this);";
                                                 }
                                             
-                                                toAppend += "<div class='element' data-name='" + name + "' data-hash='" + content[line_key][element_key][0][1] + "' data-type='" + content[line_key][element_key][0][2] + "'>";
+                                                toAppend += "<div class='element' data-name='" + name + "' data-hash='" + content[line_key][element_key][0][1] + "' data-type='" + content[line_key][element_key][0][2] + "' data-extension='" + content[line_key][element_key][0][3] + "' data-baseName='" + content[line_key][element_key][0][0] + "'>";
                                                 toAppend += "<img src='apps/app_explorer/images/types/" + content[line_key][element_key][0][2] + ".svg' onclick='app_explorer.select.trigger(this);' ondblclick='"+onclickAction+"' /><br />";
                                                 toAppend += "<div onclick='app_explorer.select.trigger(this);' ondblclick'"+onclickAction+"'>"+name+"</div>";
                                                 toAppend += "<br />";
@@ -98,6 +100,9 @@ var app_explorer =
                                 }
                                 
                                 document.querySelector("div#app_explorer div#listArea div#list").innerHTML = toAppend;
+                                
+                                app_explorer.select.showActions();
+                                app_explorer.select.showInfo();
                             }
                             catch(err)
                             {
@@ -243,6 +248,44 @@ var app_explorer =
                 toShow + "<br /><br /><span id='return_delete_"+hash+"'></span>",
                 "<input type='button' value='Supprimer' class='button' onclick='app_explorer.ajaxRequest.delete(\""+name+"\", \""+type+"\", \""+hash+"\")' />"
             );
+        },
+        
+        rename: function(callButton)
+        {
+            var element = callButton.parentNode.parentNode;
+            
+            var hash = element.getAttribute("data-hash");
+            var baseName = element.getAttribute("data-baseName");
+            var extension = element.getAttribute("data-extension");
+            var type = element.getAttribute("data-type");
+            
+            var show = "";
+            
+            if(type === "folder")
+            {
+                show = "<input type='text' placeholder='"+baseName+"' id='input_"+hash+"_1' />";
+            }
+            else
+            {
+                show = "<input type='text' placeholder='"+baseName+"' id='input_"+hash+"_1' /> . <input type='text' placeholder='"+extension+"' id='input_rename_"+hash+"_2' style='width: 5vw;' />";
+            }
+            
+            popup.open(
+                "popup_rename_"+hash,
+                "Renommer un élément",
+                show + "<br /><br /><span id='return_rename_"+hash+"'></span>",
+                "<input type='button' value='Renommer' class='button' onclick='app_explorer.ajaxRequest.rename(\""+hash+"\", \""+type+"\");' />"
+            );
+        },
+        
+        copy: function(callButton)
+        {
+            
+        },
+        
+        cut: function(callButton)
+        {
+            
         }
     },
     
@@ -366,6 +409,54 @@ var app_explorer =
             }
             
             xhr.send(null);
+        },
+        
+        /* Renommer un élément */
+        rename: function(hash, type)
+        {
+            var popup = document.querySelector("section#popup_rename_"+hash);
+            
+            var name = popup.querySelector("div.content input_"+hash+"_1").value;
+            
+            var xhr = new XMLHttpRequest();
+            
+            if(type === "folder")
+            {
+                 xhr.open("GET", "inc/ajax/explore/rename.php?hash="+hash+"&name="+name, true);
+            }
+            else
+            {
+                var extension = popup.querySelector("div.content input_"+hash+"_2").value;
+                
+                xhr.open("GET", "inc/ajax/explore/rename.php?hash="+hash+"&name="+name+"&extension="+extension, true);
+            }
+            
+            xhr.onreadystatechange = function()
+            {
+                if(xhr.status === 200 && xhr.readyState === 4)
+                {
+                    var state = xhr.responseText.split("~||]]", 1)[0];
+                    
+                    switch(state)
+                    {
+                        case "ok":
+                            app_explorer.actions.list();
+                            
+                            returnArea.innerHTML = "L'élément a été renommé avec succès en <b>" + name + "</b>";
+
+                            setTimeout(function(){
+                                popup.close("popup_rename_"+hash);
+                            }, 1000);
+                            break;
+
+                        default:
+                            returnArea.innerHTML = "Une erreur est survenue lors du renommage de l'élément en <b>" + name + "</b>";
+                            break;
+                    }
+                }
+            }
+            
+            xhr.send(null);
         }
     },
     
@@ -428,7 +519,16 @@ var app_explorer =
         /* Affiche les actions spécifiques à la sélection */
         showActions: function(nbFiles, nbFolders)
         {
-            
+            if(nbFiles + nbFolders > 1)
+            {
+                document.querySelector("#app_explorer #bandActions span.secondLine").innerHTML =    "<p class='action'><img src='apps/app_explorer/images/actions/delete.svg' /></p>"+
+                                                                                                    "<p class='action'><img src='apps/app_explorer/images/actions/copy.svg' /></p>"+
+                                                                                                    "<p class='action'><img src='apps/app_explorer/images/actions/cut.svg' /></p>";
+            }
+            else
+            {
+                document.querySelector("#app_explorer #bandActions span.secondLine").innerHTML = "";
+            }
         }
     }
 }
